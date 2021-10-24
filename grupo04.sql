420e8539315aef11438f26620c6d6bd5e5a31c01
Luis-Rivas <71407850+Luis-Rivas@users.noreply.github.com>
2021-10-24 10:19:19 -0600

    agregando script de base de datos

---
+++ grupo04.sql
@@ -0,0 +1,76 @@
+﻿# SQL-Front 5.1  (Build 4.16)
+
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
+/*!40101 SET SQL_MODE='NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
+/*!40103 SET SQL_NOTES='ON' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+
+
+# Host: localhost:3308    Database: grupo04
+# ------------------------------------------------------
+# Server version 5.5.5-10.1.13-MariaDB
+
+DROP DATABASE IF EXISTS `grupo04`;
+CREATE DATABASE `grupo04` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `grupo04`;
+
+#
+# Source for table libro
+#
+
+DROP TABLE IF EXISTS `libro`;
+CREATE TABLE `libro` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `isbn` varchar(255) DEFAULT NULL,
+  `titulo` varchar(255) DEFAULT NULL,
+  `autor` varchar(255) DEFAULT NULL,
+  `editorial` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+#
+# Dumping data for table libro
+#
+
+LOCK TABLES `libro` WRITE;
+/*!40000 ALTER TABLE `libro` DISABLE KEYS */;
+INSERT INTO `libro` VALUES (1,'123456','La Iliada','Homero','Editorial X');
+INSERT INTO `libro` VALUES (2,'23467','La Odisea','Homero','Editorial X');
+INSERT INTO `libro` VALUES (3,'345678','El Principito','Antoine de Saint-Exupéry','Le Siecle');
+INSERT INTO `libro` VALUES (4,'456789','Don Quijote','Miguel de Cervantes','Francisco de Robles');
+/*!40000 ALTER TABLE `libro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+#
+# Source for table usuario
+#
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE `usuario` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `clave` varchar(255) DEFAULT NULL,
+  `nivel` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+#
+# Dumping data for table usuario
+#
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Digitador','Adán','asd123','1');
+INSERT INTO `usuario` VALUES (2,'Mantenimiento','Fatima','asd123','2');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
